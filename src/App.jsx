eac3b45ea96365649754d85892798f6dd24290a1
geekabhinav007 <abhinav26january@gmail.com>
2024-04-25 03:18:18 +0530

    Structure created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
 
 import './App.css'
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
 
 function App() {
 
   return (
     <>
-     
+     <p>hello</p>
     </>
   )
 }
