f531732e0de76efc4ad1a16620ade8554cd25070
geekabhinav007 <abhinav26january@gmail.com>
2024-04-25 05:18:46 +0530

    Auth protection implemented

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
-import { signOut } from 'firebase/auth'
+import React, { useEffect } from 'react'
+import { signOut, onAuthStateChanged } from 'firebase/auth'
 import { auth } from '../firebase'
 import { useNavigate } from 'react-router-dom'
 
 function Dashboard() {
 
     const navigate = useNavigate()
+
+
+    // handle protection of unauthorised access of auth route
+    useEffect(() => {
+
+        auth.onAuthStateChanged(user => {
+            if (!user) {
+                navigate('/')
+            }
+        })
+    })
 
     // Logout Implementation
 
