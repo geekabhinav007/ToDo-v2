6b9960d362cf085557b87018214be8ab6b85986f
geekabhinav007 <abhinav26january@gmail.com>
2024-04-25 04:01:08 +0530

    Route setup  using react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,25 @@
 import './App.css'
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
 
   return (
-    <>
-     <p>hello</p>
-    </>
+  <div className="app">
+      <Router>
+        <Routes>
+          <Route path="/" element={< Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
+     </div>
   )
 }
 
-export default App
+export default App;
