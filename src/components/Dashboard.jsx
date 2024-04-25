43a4e3d9643c3e0295c30d0bfa0aa923a57c37dc
geekabhinav007 <abhinav26january@gmail.com>
2024-04-25 12:41:20 +0530

    Todo List functionality Implemented

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,22 +1,23 @@
-import React, { useEffect } from 'react'
-import { signOut, onAuthStateChanged } from 'firebase/auth'
-import { auth } from '../firebase'
-import { useNavigate } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { signOut } from "firebase/auth";
+import { auth, db } from "../firebase.js";
+import { useNavigate } from "react-router-dom";
+import { uid } from "uid";
+import { set, ref, onValue, remove, update } from "firebase/database";
+
 
 function Dashboard() {
 
-    const navigate = useNavigate()
+
+    const [todo, setTodo] = useState("");
+    const [todos, setTodos] = useState([]);
+    const [isEdit, setIsEdit] = useState(false);
+    const [tempUidd, setTempUidd] = useState("");
+    const navigate = useNavigate();
 
 
-    // handle protection of unauthorised access of auth route
-    useEffect(() => {
 
-        auth.onAuthStateChanged(user => {
-            if (!user) {
-                navigate('/')
-            }
-        })
-    })
+
 
     // Logout Implementation
 
@@ -29,19 +30,107 @@ function Dashboard() {
             })
     }
 
+    // handle protection of unauthorised access of auth route
+
+    useEffect(() => {
+        auth.onAuthStateChanged((user) => {
+            if (user) {
+                // read
+                onValue(ref(db, `/${auth.currentUser.uid}`), (snapshot) => {
+                    const data = snapshot.val();
+                    if (data !== null) {
+                        const newTodos = [];
+                        Object.values(data).forEach((todo) => {
+                            newTodos.push(todo);
+                        });
+                        setTodos(newTodos);
+                    }
+                });
+            } else if (!user) {
+                navigate("/");
+            }
+        });
+    }, [navigate]);
+
+
+    // add
+    const writeToDatabase = () => {
+        const uidd = uid();
+        set(ref(db, `/${auth.currentUser.uid}/${uidd}`), {
+            todo: todo,
+            uidd: uidd
+        });
+
+        setTodo("");
+    };
+
+    // update
+    const handleUpdate = (todo) => {
+        setIsEdit(true);
+        setTodo(todo.todo);
+        setTempUidd(todo.uidd);
+    };
+
+    const handleEditConfirm = () => {
+        update(ref(db, `/${auth.currentUser.uid}/${tempUidd}`), {
+            todo: todo,
+            tempUidd: tempUidd
+        });
+
+        setTodo("");
+        setIsEdit(false);
+    };
+
+    // delete
+    const handleDelete = (uid) => {
+        remove(ref(db, `/${auth.currentUser.uid}/${uid}`));
+    };
+
+
 
     return (
-        <>
+        <section className="bg-gray-50 dark:bg-gray-900">
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                <a className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+                    <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo" />
+                    Welcome
+                </a>
+                <div className="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
+                    <div class="mb-4">
 
-
-            <h1>DashBoard</h1>
-
-            <button
-                onClick={handleSignOut}
-            >
-                SignOut</button>
-        </>
+                        <div class="flex mt-4">
+                            <input
+                                class="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker"
+                                type="text"
+                                placeholder="Add todo..."
+                                value={todo}
+                                onChange={(e) => setTodo(e.target.value)}
+                            />
+                            {isEdit ? (
+                                <button onClick={handleEditConfirm} class=" text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 flex-no-shrink p-2 border-2  ">Update</button>
+                            ) : (
+                                <button onClick={writeToDatabase} class=" text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 flex-no-shrink p-2 border-2  ">Add</button>
+                            )}
+                        </div>
+                    </div>
+                    <div>
+                        {todos.map((todo) => (
+                            <div class="flex mb-4 items-center">
+                                <p class="w-full text-grey-darkest">{todo.todo}</p>
+                                <div class="flex flex-row flex-nowrap">
+                                    <button onClick={() => handleUpdate(todo)} class=" text-white bg-green-600 hover:bg-green-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 flex-no-shrink p-2 border-2 ">Edit</button>
+                                    <button onClick={() => handleDelete(todo.uidd)} class=" text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 flex-no-shrink p-2 border-2 ">Delete</button>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                    <button onClick={handleSignOut} class=" mt-5 text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 flex-no-shrink p-2 border-2 cursor-pointer">Logout</button>
+                </div>
+            </div>
+        </section>
     )
+
+
 
 }
 
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { signInWithEmailAndPassword } from 'firebase/auth'
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth'
 import { auth } from '../firebase'
 import { useNavigate } from 'react-router-dom'
 
@@ -39,10 +39,11 @@ function Login() {
     // Sigin Button 
 
     const handleSignIn = () => {
+        event.preventDefault();
+
         signInWithEmailAndPassword(auth, email, password)
             .then(() => {
                 navigate("/dashboard");
-                event.preventDefault();
 
             })
             .catch(
@@ -56,6 +57,8 @@ function Login() {
     // User Registration function
 
     const handleRegister = () => {
+        event.preventDefault();
+
         if (registerInformation.email !== registerInformation.confirmEmail) {
             alert("Please confirm that email are the same");
             return;
@@ -71,49 +74,81 @@ function Login() {
             registerInformation.password
         )
             .then(() => {
-                navigate("/homepage");
+                navigate("/dashboard");
             })
             .catch((err) => alert(err.message));
     };
 
+    // Returning JSX
+
     return (
         <section className="bg-gray-50 dark:bg-gray-900">
             <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+                <a className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
                     <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo" />
                     ToDo List      </a>
                 <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                     {isRegistering ? (
                         <>
 
-                            <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-                                <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                                    Create and account
+                            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                                <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                                    Create account
                                 </h1>
-                                <form class="space-y-4 md:space-y-6" action="#">
+                                <form className="space-y-4 md:space-y-6" >
                                     <div>
-                                        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                                        <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="" />
+                                        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                                        <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required=""
+                                            value={registerInformation.email}
+                                            onChange={(e) =>
+                                                setRegisterInformation({
+                                                    ...registerInformation,
+                                                    email: e.target.value
+                                                })
+                                            }
+                                        />
                                     </div>
                                     <div>
-                                        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm email</label>
-                                        <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="" />
+                                        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm email</label>
+                                        <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required=""
+                                            value={registerInformation.confirmEmail}
+                                            onChange={(e) =>
+                                                setRegisterInformation({
+                                                    ...registerInformation,
+                                                    confirmEmail: e.target.value
+                                                })
+                                            } />
                                     </div>
                                     <div>
-                                        <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                                        <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+                                        <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                                        <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""
+                                            value={registerInformation.password}
+                                            onChange={(e) =>
+                                                setRegisterInformation({
+                                                    ...registerInformation,
+                                                    password: e.target.value
+                                                })
+                                            } />
                                     </div>
                                     <div>
-                                        <label for="confirm-password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
-                                        <input type="confirm-password" name="confirm-password" id="confirm-password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+                                        <label htmlFor="confirm-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
+                                        <input type="confirm-password" name="confirm-password" id="confirm-password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""
+                                            value={registerInformation.confirmPassword}
+                                            onChange={(e) =>
+                                                setRegisterInformation({
+                                                    ...registerInformation,
+                                                    confirmPassword: e.target.value
+                                                })
+                                            } />
                                     </div>
 
-                                    <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                                    <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                                         onClick={handleRegister}
 
                                     >Create an account</button>
-                                    <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                                        Already have an account? <a href="" class="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                                    <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                                        Already have an account? <a className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                                            onClick={() => setIsRegistering(false)}
                                         >Login here</a>
                                     </p>
                                 </form>
@@ -126,7 +161,7 @@ function Login() {
                                     <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                                         Sign in to your account
                                     </h1>
-                                    <form className="space-y-4 md:space-y-6" action="#">
+                                    <form className="space-y-4 md:space-y-6">
                                         <div>
                                             <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
                                             <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required=""
@@ -142,11 +177,11 @@ function Login() {
                                             />
                                         </div>
 
-                                        <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                                        <button type="submit" className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                                             onClick={handleSignIn}
                                         >Sign in</button>
                                         <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                                            Don’t have an account yet? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                                            Don’t have an account yet? <a className="font-medium text-primary-600 hover:underline dark:text-primary-500"
                                                 onClick={() => setIsRegistering(true)}
                                             >Sign up</a>
                                         </p>
