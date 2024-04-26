4c096add5f794a42815525ea382babd243bf08be
geekabhinav007 <abhinav26january@gmail.com>
2024-04-26 10:24:42 +0530

    fix look of search input

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -92,13 +92,16 @@ function Dashboard() {
 
     return (
         <section className="bg-gray-50 dark:bg-gray-900 h-screen">
-            <input
-                class="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker"
-                type="text"
-                placeholder="Search todos..."
-                value={searchTerm}
-                onChange={(e) => setSearchTerm(e.target.value)}
-            />
+            <div class=" flex justify-center">
+    <input
+        class=" m-5 shadow appearance-none border rounded w-1/2 py-2 px-3 text-grey-darker"
+        type="text"
+        placeholder="Search todos..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+    />
+</div>
+
 
             <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
                 <a className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
